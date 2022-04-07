3d8bee23307a51110b94937e975af6f29454045f
gtsompanoglou <georgiostsobys@gmail.com>
2022-04-07 20:41:08 +0300

    init commit2

--- app/src/main/java/com/appena/uniapp/MainActivity.java
+++ app/src/main/java/com/appena/uniapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.appena.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(){
+        Intent intent = new Intent(this,secondActivity.class);
+    }
+
 }
---
+++ app/src/main/java/com/appena/uniapp/selectContinent.java
@@ -0,0 +1,14 @@
+package com.appena.uniapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectContinent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_continent);
+    }
+}
