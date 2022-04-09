2f4d289a12274c750e9df2276227facb8e75c9a9
gtsompanoglou <georgiostsobys@gmail.com>
2022-04-09 21:46:17 +0300

    buttons of countries are layed ok

--- app/src/main/java/com/appena/uniapp/Continents.java
+++ app/src/main/java/com/appena/uniapp/Continents.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class selectContinent extends AppCompatActivity {
+public class Continents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_continent);
+        setContentView(R.layout.activity_continents);
     }
 }
--- app/src/main/java/com/appena/uniapp/MainActivity.java
+++ app/src/main/java/com/appena/uniapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(){
-        Intent intent = new Intent(this,secondActivity.class);
+    public void switchActivity(View view){
+        Intent intentToContinent = new Intent(this, Continents.class);
+        startActivity(intentToContinent);
     }
 
-}
+}//end of class
