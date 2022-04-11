bf0841f684150b358c9c7dedd8fcb9c5c191e625
gtsompanoglou <georgiostsobys@gmail.com>
2022-04-11 17:54:25 +0300

    v1.0:READY!

--- app/src/main/java/com/appena/uniapp/Greece.java
+++ app/src/main/java/com/appena/uniapp/Greece.java
@@ -3,8 +3,11 @@ package com.appena.uniapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import java.net.URI;
 
 public class Greece extends AppCompatActivity {
 
@@ -14,4 +17,22 @@ public class Greece extends AppCompatActivity {
         setContentView(R.layout.activity_greece);
     }
 
+    public void gotoSite(View view){
+        String url =  "http://www.dib.uth.gr";
+        Uri webpage = Uri.parse(url);
+        Intent webIntent = new Intent(Intent.ACTION_VIEW);
+        webIntent.setData(webpage);
+        if(webIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(webIntent);
+        }
+
+    }
+
+    public void gotoMap(View view){
+        Uri location = Uri.parse("geo:38.91309966134338, 22.427587681605267?z=160");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
+        if(mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+    }
 }//end of class
